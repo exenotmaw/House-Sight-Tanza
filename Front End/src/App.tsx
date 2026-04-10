e2337cad2d9e6b5a938901546d450630432810a0
exenotmaw <anonymous.raizel@gmail.com>
2026-04-10 21:15:00 +0800

    Add Vercel Analytics tracking

--- Front End/src/App.tsx
+++ Front End/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
 import Studio from './pages/PredictionStudio/Studio';
 import Analysis from './pages/AnalysisPage/Analysis';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/studio" element={<Studio />} />
         <Route path="/analysis" element={<Analysis />} />
       </Routes>
+      <Analytics /> 
     </Router>
     
   );
